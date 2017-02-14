492c6efdf448e2473139a5df8808222ceb8555ef
АманкельдыЖ <@306_03_pc.local.itstep>
2017-02-14 18:06:14 +0300

    first student commit

---
+++ src/main/java/kz/azh/app/list/Student.java
@@ -0,0 +1,20 @@
+package kz.azh.app.list;
+
+public class Student {
+	
+	private String name;
+	private String group;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		name = name;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		group = group;
+	}
+}
